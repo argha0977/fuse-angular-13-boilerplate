6dec422a2b91a60b3a2b79df62fb5dd06882ea96
Argha <email2argha@gmail.com>
2021-11-08 18:07:53 +0530

    Routing file added for users

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { UsersComponent } from './users.component';
 import { FuseCardModule } from '@fuse/components/card';
 import { FuseAlertModule } from '@fuse/components/alert';
 import { SharedModule } from 'app/shared/shared.module';
-
-const usersRoutes: Route[] = [
-  {
-    path: '',
-    component: UsersComponent
-  }
-]
+import { usersRoutes } from './users.routing';
 
 @NgModule({
   declarations: [
